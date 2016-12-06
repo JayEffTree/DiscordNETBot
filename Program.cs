f3d6046978b9b9a8ac4a8ab3e4c07ea372fdf39c
Jay Tree <jayefftree@hotmail.com>
2016-12-05 18:54:19 -0700

    Initial Push

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace TestNETBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiscordClient client = new DiscordClient("", true);
+
+            client.MessageReceived += (sender, e) => // Channel message has been received
+            {
+                if (e.MessageText.StartsWith(".ping"))
+                {
+                    e.Channel.SendMessage("Pong, " + e.Author.Mention);
+                }
+            };
+
+        Console.WriteLine("Attempting to connect!");
+
+            try
+            {
+                client.SendLoginRequest();
+                client.Connect();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            client.Connected += (sender, e) =>
+            {
+                Console.WriteLine("CLIENT CONNECTED");
+            };
+
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
